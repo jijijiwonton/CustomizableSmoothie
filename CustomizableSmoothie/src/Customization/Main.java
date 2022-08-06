03fb27941017b8a6d133fa3dd84802ea87702abc
jiwonhwang <jijijiwonton@github.com>
2022-08-05 22:13:25 -0400

    CS-3 Harden Order class with customized exception

--- CustomizableSmoothie/src/Customization/Main.java
+++ CustomizableSmoothie/src/Customization/Main.java
@@ -2,7 +2,7 @@ package Customization;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws OutOfMenuException {
 
 		Order o = new Order();
 		o.OrderSmoothie();
