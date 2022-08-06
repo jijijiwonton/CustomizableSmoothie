522f085259a8ad02eb45cf584f9cfc60eefaed4e
jiwonhwang <jijijiwonton@github.com>
2022-08-06 15:49:44 -0400

    CS-8  Add Junit test for NonCustomization class

---
+++ CustomizableSmoothie/src/Customization/NonCustomizationTest.java
@@ -0,0 +1,67 @@
+package Customization;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class NonCustomizationTest {
+	
+	NonCustomization nc = new NonCustomization();
+
+	/**
+	 * Verify menu() to display all available non-customized menu
+	 */
+	@Test
+	void testMenu() {
+		String[] menuOptions = {"EchoOfDevils", "GreenParty", "GreenSun", "HappyDessert", "RedParty", "YellowParty", "VitaminBooster"};
+		
+		List<String> actual = new ArrayList<String>();
+		List<String> expected = new ArrayList<String>();
+		
+		// Actual non-customized menu options
+		for(int i = 0; i < menuOptions.length; i++) {
+			actual.add(menuOptions[i]);
+		}
+		
+		// Expected non-customized menu options
+		for(int i = 0; i < nc.menu().size(); i++) {
+			expected.add(nc.menu().get(i).toString());
+		}
+		
+		// Verify menu()
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Verify getting non-customized smoothie details
+	 * smoothie name, smoothie ingredients, smoothie calories, smoothie price 
+	 */
+	@Test
+	void testGetNonCusomizedSmoothieDetails() {
+		// Actual smoothie information
+		String actualName = "VitaminBooster";
+		String[] actualIngredients = {"BloodOrange", "Lemon", "Apricot", "Peach"};
+		double actualCalories = 77;
+		double actualPrice = 5.3;
+		
+		nc.getNonCusomizedSmoothieDetails(NonCustomaziable.VitaminBooster);
+		
+		String actualDetails = "***** Non customized smoothie *****\n"
+				+ "Smoothie    : VitaminBooster\n"
+				+ "Ingredients : [BloodOrange, Lemon, Apricot, Peach]\n"
+				+ "Calories    : 77.0\n"
+				+ "Price       : $5.3";
+		
+		String expectedDetails = "***** Non customized smoothie *****" + "\n" +
+				   "Smoothie    : " + actualName + "\n" +
+				   "Ingredients : " + Arrays.asList(actualIngredients) + "\n" + 
+				   "Calories    : " + actualCalories + "\n" +
+				   "Price       : " + "$" + actualPrice;
+		
+		assertEquals(expectedDetails, actualDetails);
+	}
+}
